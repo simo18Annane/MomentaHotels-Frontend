242b957ee4a591b3fc236b9dee0eda0096babf07
Mohamed Annane <mohamed.annane01@gmail.com>
2025-11-19 01:21:37 +0100

    homepage desgined

--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HerosectionComponent } from "../herosection/herosection.component";
 import { CitiessectionComponent } from "../citiessection/citiessection.component";
 import { ServicessectionComponent } from "../servicessection/servicessection.component";
+import { AboutussectionComponent } from "../aboutussection/aboutussection.component";
+import { ContactsectionComponent } from "../contactsection/contactsection.component";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HerosectionComponent, CitiessectionComponent, ServicessectionComponent],
+  imports: [HerosectionComponent, CitiessectionComponent, ServicessectionComponent, AboutussectionComponent, ContactsectionComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
